40f4fd384222594656c0679761150d35aa4e2eb3
Nick Ingolia <nick@ingolia.org>
2016-09-25 18:15:24 -0700

    Refactored general functions into schutil

--- d1p1.c
+++ d1p1.c
@@ -9,6 +9,8 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
 
+#include "schutil.h"
+
 #define NPTS 255
 #define MIDDLE ((NPTS+1)/2)
 #define STATESIZE (NPTS + 2)
@@ -17,12 +19,6 @@
 #define HSTEP (1.0/64.0)
 
 void solve_potential(const char *prefix, const gsl_vector *V);
-void eig_table(gsl_matrix **Psis, gsl_vector **Es, const gsl_vector *eval, const gsl_matrix *evec);
-void write_potential(const char *prefix, const gsl_vector *V);
-void write_hermitian(const char *prefix, const gsl_matrix *H);
-void write_energies(const char *prefix, const gsl_matrix *Psis, const gsl_vector *Es);
-void write_psi(const char *prefix, const gsl_matrix *Psis, const gsl_vector *Es);
-void set_H(gsl_matrix *H, const gsl_vector *V);
 
 int main(void)
 {
@@ -48,9 +44,9 @@ void solve_potential(const char *prefix, const gsl_vector *V)
 
   gsl_matrix *H = gsl_matrix_alloc(STATESIZE, STATESIZE);
 
-  set_H(H, V);
-
-  write_hermitian(prefix, H);
+  set_hamiltonian(H, V, MASS, HSTEP);
+  check_symmetry(H);
+  write_hamiltonian(prefix, H);
 
   gsl_vector *eval = gsl_vector_alloc(STATESIZE);
   gsl_matrix *evec = gsl_matrix_alloc(STATESIZE, STATESIZE);
@@ -62,149 +58,8 @@ void solve_potential(const char *prefix, const gsl_vector *V)
 
   gsl_eigen_symmv_sort(eval, evec, GSL_EIGEN_SORT_VAL_ASC);
 
-  gsl_matrix *Psis;
-  gsl_vector *Es;
-
-  eig_table(&Psis, &Es, eval, evec);
-  write_energies(prefix, Psis, Es);
-  write_psi(prefix, Psis, Es);
+  write_energies(prefix, evec, eval);
+  write_psi(prefix, evec, eval);
 }
 
-void set_H(gsl_matrix *H, const gsl_vector *V)
-{
-  gsl_matrix_set_all(H, 0.0);
-
-  for (int j = 1; j <= NPTS; j++) {
-    const double pfact = -0.5 / MASS;
-    const double hstep2 = 1.0 / (HSTEP * HSTEP);
-
-    if (j > 1)    { gsl_matrix_set(H, j, j-1, pfact * hstep2); }
-    if (j < NPTS) { gsl_matrix_set(H, j, j+1, pfact * hstep2); }
-
-    gsl_matrix_set(H, j, j, -2.0 * pfact * hstep2 + gsl_vector_get(V, j));
-  }
-}
-
-void eig_table(gsl_matrix **Psis, gsl_vector **Es, const gsl_vector *eval, const gsl_matrix *evec)
-{
-  (*Psis) = gsl_matrix_alloc(STATESIZE, STATESIZE);
-  (*Es) = gsl_vector_alloc(STATESIZE);
-
-  gsl_vector *psi = gsl_vector_alloc(STATESIZE);
-
-  for (int st = 0; st < STATESIZE; st++) {
-    gsl_matrix_get_col(psi, evec, st);
-    gsl_matrix_set_col(*Psis, st, psi);
-    gsl_vector_set(*Es, st, gsl_vector_get(eval, st));
-    printf("E_%d\t%0.4f\n", st, gsl_vector_get(eval, st));
-  }
-  gsl_vector_free(psi);
-}
-
-void write_potential(const char *prefix, const gsl_vector *V)
-{
-  char *stname;
-  if (asprintf(&stname, "%s_V.txt", prefix) < 0) { exit(1); }
-  FILE *fpot;
-  if ((fpot = fopen(stname, "w")) == NULL) {
-    fprintf(stderr, "Cannot open output file \"%s\"\n", stname);
-    exit(1);
-  }
-  free(stname);
-
-  for (int j = 1; j <= NPTS; j++) {
-    fprintf(fpot, "%d\t%0.6f\n", j, gsl_vector_get(V, j));
-  }
-
-  fclose(fpot);
-}
-
-void write_hermitian(const char *prefix, const gsl_matrix *H)
-{
-  FILE *fout;
-
-  char *stname;
-  if (asprintf(&stname, "%s_H.txt", prefix) < 0) { exit(1); }
-  if ((fout = fopen(stname, "w")) == NULL) {
-    fprintf(stderr, "Cannot open output file \"%s\"\n", stname);
-    exit(1);
-  }
-  free(stname);
-
-  fprintf(fout, "j");
-  for (int k = 0; k < H->size2; k++) {
-    fprintf(fout, "\tk_%d", k);
-  }
-  fprintf(fout, "\n");
-  
-  for (int j = 0; j < H->size1; j++) {
-    fprintf(fout, "%d", j);
-
-    for (int k = 0; k < H->size2; k++) {
-      fprintf(fout, "\t%0.6f", gsl_matrix_get(H, j, k));
-    }
-
-    fprintf(fout, "\n");
-  }
-
-  
-  fclose(fout);
-}
-
-void write_energies(const char *prefix, const gsl_matrix *Psis, const gsl_vector *Es)
-{
-  FILE *fout;
-
-  char *stname;
-  if (asprintf(&stname, "%s_E.txt", prefix) < 0) { exit(1); }
-  if ((fout = fopen(stname, "w")) == NULL) {
-    fprintf(stderr, "Cannot open output file \"%s\"\n", stname);
-    exit(1);
-  }
-  free(stname);
-
-  gsl_vector *psi = gsl_vector_alloc(STATESIZE);
-
-  fprintf(fout, "phi_n\tE_n\n");
-  for (int st = 0; st + 1 < Es->size; st++) {
-    fprintf(fout, "%d\t%0.6f", st, gsl_vector_get(Es, st));      
-    fprintf(fout, "\n");
-  }
-
-  gsl_vector_free(psi);
-  fclose(fout);
-}
-
-void write_psi(const char *prefix, const gsl_matrix *Psis, const gsl_vector *Es)
-{
-  FILE *fout;
-
-  char *stname;
-  if (asprintf(&stname, "%s_psi.txt", prefix) < 0) { exit(1); }
-  if ((fout = fopen(stname, "w")) == NULL) {
-    fprintf(stderr, "Cannot open output file \"%s\"\n", stname);
-    exit(1);
-  }
-  free(stname);
-
-  fprintf(fout, "j");
-  for (int st = 0; st < Es->size; st++) {
-    fprintf(fout, "\tpsi_%d", st);
-  }
-  fprintf(fout, "\n");
-  
-  for (int j = 0; j < STATESIZE; j++) {
-    fprintf(fout, "%d", j);
-
-    for (int st = 0; st < Psis->size2; st++) {
-      double psi_st_j = gsl_matrix_get(Psis, j, st);
-      fprintf(fout, "\t%0.6f", psi_st_j);
-    }
-
-    fprintf(fout, "\n");
-  }
-
-  
-  fclose(fout);
-}
 
