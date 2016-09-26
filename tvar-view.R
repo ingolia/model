43fe3d4b592588ee0bd5eef83a3816ea9fb0d9d7
Nick Ingolia <nick@ingolia.org>
2016-09-25 22:00:25 -0700

    Useful R visualizations including animated PDF and colormap

---
+++ tvar-view.R
@@ -0,0 +1,32 @@
+# psi0 <- read.delim("psi-v0-t.txt", header=FALSE, row.names=1)
+psi1 <- read.delim("psi-v1-t.txt", header=FALSE, row.names=1)
+
+#yrange <- c(0, 1.1*max(psi0, psi1))
+yrange <- c(0, 1.1*max(psi1))
+#plot(t(psi0)[,1], type="l", col="black", ylim=yrange)
+
+plot(t(psi1)[,1], type="l", col="black", ylim=yrange)
+lines(t(psi1)[,256], col="blue")
+lines(t(psi1)[,512], col="cyan")
+lines(t(psi1)[,768], col="green")
+lines(t(psi1)[,1024], col="red")
+lines(t(psi1)[,2048], col="magenta")
+
+plot(psi1$V64, type="l", col="black", ylim=yrange)
+lines(psi1$V32, col="blue")
+lines(psi1$V96, col="red")
+
+image(z=as.matrix(psi1), zlim=yrange, col=topo.colors(32))
+
+pdf("animation.pdf", useDingbats=FALSE)
+for (i in seq(20,2560,10)) {
+  plot(t(psi1)[,i], type="l", ylim=yrange, lwd=2)
+  for (j in seq(i-1,i-10,-1)) {
+    lines(t(psi1)[,j])
+  }
+  for (j in seq(i-11,i-19,-1)) {
+    lines(t(psi1)[,j], col="grey")
+  }
+  title(main=sprintf("%d to %d", i, i-19))
+}
+dev.off()
