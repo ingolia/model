8fd7703a5b617679df563a95c18031631feb74bd
Nick Ingolia <nick@ingolia.org>
2018-03-03 15:12:07 -0800

    Abstracted terminal plotting. Downsample vectors to fit.

--- writing.c
+++ writing.c
@@ -8,6 +8,9 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_vector_complex_double.h>
 
+#define MIN_ABS2 1e-6
+static void terminal_graph_raw(const gsl_vector *v, const unsigned int nlines, char filled);
+
 void fwrite_vector(FILE *f, const gsl_vector *V)
 {
   for (int j = 0; j < V->size; j++) {
@@ -69,53 +72,48 @@ void terminal_graph_abs(const gsl_vector_complex *psi, const int nlines, const d
 {
   gsl_vector *h = gsl_vector_alloc(psi->size);
   for (int i = 0; i < psi->size; i++) {
-    gsl_vector_set(h, i, gsl_complex_abs(gsl_vector_complex_get(psi, i)));
+    gsl_vector_set(h, i, gsl_complex_abs(gsl_vector_complex_get(psi, i)) / ymax);
   }
 
-  double lineheight = ymax / ((double) nlines);
-  
-  for (int j = nlines; j > 0; j--) {
-    double hthresh = (((double) j) - 0.5) * lineheight;
-    printf("%0.4f |", hthresh);
-    for (int i = 0; i < psi->size; i++) {
-      if (gsl_vector_get(h, i) > hthresh) {
-        putchar('#');
-      } else {
-        putchar(' ');
-      }
-    }
-    puts("|");
-  }  
+  terminal_graph_raw(h, nlines, '#');
+  gsl_vector_free(h);
 }
 
 void terminal_graph_abs2(const gsl_vector_complex *psi, const int nlines, const double ymax)
 {
   gsl_vector *h = gsl_vector_alloc(psi->size);
   for (int i = 0; i < psi->size; i++) {
-    gsl_vector_set(h, i, gsl_complex_abs2(gsl_vector_complex_get(psi, i)));
+    gsl_vector_set(h, i, gsl_complex_abs2(gsl_vector_complex_get(psi, i)) / ymax);
   }
 
-  double lineheight = ymax / ((double) nlines);
-  
+  terminal_graph_raw(h, nlines, '*');
+  gsl_vector_free(h);
+}
+
+void terminal_graph_raw(const gsl_vector *v, const unsigned int nlines, char filled) {
+  const double lineheight = 1.0 / ((double) nlines);
+
   for (int j = nlines; j > 0; j--) {
     double hthresh = (((double) j) - 0.5) * lineheight;
     printf("%0.4f |", hthresh);
-    for (int i = 0; i < psi->size; i++) {
-      if (gsl_vector_get(h, i) > hthresh) {
-        putchar('*');
+    for (int i = 0; i < v->size; i++) {
+      if (gsl_vector_get(v, i) > hthresh) {
+        putchar(filled);
       } else {
         putchar(' ');
       }
     }
-    puts("|");
+    puts("|");    
   }
 }
 
 void terminal_graph_phase(const gsl_vector_complex *psi, const int nlines)
 {
   gsl_vector *h = gsl_vector_alloc(psi->size);
+  gsl_vector *g = gsl_vector_alloc(psi->size);
   for (int i = 0; i < psi->size; i++) {
     gsl_vector_set(h, i, gsl_complex_arg(gsl_vector_complex_get(psi, i)) + M_PI);
+    gsl_vector_set(g, i, gsl_complex_abs2(gsl_vector_complex_get(psi, i)));
   }
 
   double lineheight = (2.0 * M_PI) / ((double) nlines);
@@ -124,7 +122,8 @@ void terminal_graph_phase(const gsl_vector_complex *psi, const int nlines)
     double linemax = (((double) j)      ) * lineheight;
     printf("%0.4f |", linemin);
     for (int i = 0; i < psi->size; i++) {
-      if (gsl_vector_get(h, i) > linemin && gsl_vector_get(h, i) <= linemax) {
+      if ((gsl_vector_get(g, i) > MIN_ABS2) &&
+	(gsl_vector_get(h, i) > linemin && gsl_vector_get(h, i) <= linemax)) {
         putchar('-');
       } else {
         putchar(' ');
@@ -156,4 +155,20 @@ void fwrite_vector_complex_arg(FILE *f, const gsl_vector_complex *psi)
     fprintf(f, "\t%0.3f", gsl_complex_arg(gsl_vector_complex_get(psi, j)));
   }
   fprintf(f, "\n");
+}
+
+void downsample_vector_complex_alloc(gsl_vector_complex **vv, const gsl_vector_complex *v, size_t n)
+{
+  size_t vvsize = v->size / n;
+
+  *vv = gsl_vector_complex_alloc(vvsize);
+
+  const double ninv = 1.0 / ((double) n);
+  for (size_t i = 0; i < vvsize; i++) {
+    gsl_complex avg = gsl_vector_complex_get(v, i*n);
+    for (size_t j = 1; j < n; j++) {
+      avg = gsl_complex_add(avg, gsl_vector_complex_get(v, (i*n) + j));
+    }
+    gsl_vector_complex_set(*vv, i, gsl_complex_mul_real(avg, ninv));
+  }
 }
--- writing.h
+++ writing.h
@@ -11,6 +11,8 @@ void fwrite_vector_complex_arg(FILE *f, const gsl_vector_complex *psi);
 void fwrite_matrix_complex(FILE *f, const gsl_matrix_complex *M);
 void fwrite_matrix(FILE *f, const gsl_matrix *M);
 
+void downsample_vector_complex_alloc(gsl_vector_complex **vv, const gsl_vector_complex *v, unsigned int n);
+
 void terminal_graph_abs(const gsl_vector_complex *psi, const int, const double);
 void terminal_graph_abs2(const gsl_vector_complex *psi, const int, const double);
 void terminal_graph_phase(const gsl_vector_complex *psi, const int);
