3ae12fa728dca1b87654fca4bfff48c6c5f96836
Nick Ingolia <nick@ingolia.org>
2018-04-19 00:23:01 -0700

    More modularity and better names for fading potentials in and out

--- circular.c
+++ circular.c
@@ -28,10 +28,7 @@
 #define HSTEP (LENGTH / ((double) NPTS))
 #define TSTEP (1.0/4096.0)
 
-// psi1(x) = a * sin(x)
-// <psi1'|psi1> = a^2 * M_PI => a = 1/sqrt(pi) = 1.0 / M_SQRTPI 
-
-#define WRITEEVERY 512
+#define WRITEEVERY 256
 
 #define NSTEPS 32
 
@@ -39,38 +36,54 @@ typedef struct {
   timeevol_halves *Usteady[NSTEPS];
   timeevol_halves *Uupfrom[NSTEPS];
   timeevol_halves *Udownto[NSTEPS];
-} precomputed_timeevol;
+} fade_timeevol;
 
 void test_6pts(void);
 void solve_stationary(const gsl_vector *, gsl_matrix *);
-void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const precomputed_timeevol *Uall);
+void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const fade_timeevol *Uall);
 
-precomputed_timeevol *precompute(gsl_matrix *const Hs[NSTEPS]);
+fade_timeevol *make_fade_timeevol(gsl_matrix *const Hs[NSTEPS]);
 
 void v_norm_ground(gsl_vector *V);
 void v_sin_6pt(gsl_vector *V, double Vpts[6]);
-void v_sin(gsl_vector *V, double scale);
 
 int main(void)
 {
   //  test_6pts();
+
+  double Vpts[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
   
-  gsl_vector **Vs = calloc(NSTEPS, sizeof(gsl_vector *));
-  gsl_matrix **Hs = calloc(NSTEPS, sizeof(gsl_matrix *));
+  gsl_vector **V1s = calloc(NSTEPS, sizeof(gsl_vector *));
+  gsl_matrix **H1s = calloc(NSTEPS, sizeof(gsl_matrix *));
   for (int i = 0; i < NSTEPS; i++) {
-    Vs[i] = gsl_vector_calloc(STATESIZE);
-    v_sin(Vs[i], V0MAX * ((double) i) / ((double) (NSTEPS - 1)));
-    v_norm_ground(Vs[i]);
-    Hs[i] = gsl_matrix_alloc(STATESIZE, STATESIZE);
-    set_hamiltonian_circular(Hs[i], Vs[i], PLANCK, MASS, HSTEP);
+    V1s[i] = gsl_vector_calloc(STATESIZE);
+    Vpts[0] = Vpts[2] = Vpts[2] = Vpts[3] = Vpts[4] = Vpts[5] = 0.0;
+    Vpts[1] = V0MAX * ((double) i) / ((double) (NSTEPS - 1));
+    v_sin_6pt(V1s[i], Vpts);
+    v_norm_ground(V1s[i]);
+    H1s[i] = gsl_matrix_alloc(STATESIZE, STATESIZE);
+    set_hamiltonian_circular(H1s[i], V1s[i], PLANCK, MASS, HSTEP);
   }
 
-  solve_stationary(Vs[0], Hs[0]);
-  solve_stationary(Vs[NSTEPS-1], Hs[NSTEPS-1]);
-  
-  precomputed_timeevol *Uall = precompute(Hs);
+  gsl_vector **V2s = calloc(NSTEPS, sizeof(gsl_vector *));
+  gsl_matrix **H2s = calloc(NSTEPS, sizeof(gsl_matrix *));
+  for (int i = 0; i < NSTEPS; i++) {
+    V2s[i] = gsl_vector_calloc(STATESIZE);
+    Vpts[0] = Vpts[1] = Vpts[2] = Vpts[3] = Vpts[4] = Vpts[5] = 0.0;
+    Vpts[2] = V0MAX * ((double) i) / ((double) (NSTEPS - 1));
+    v_sin_6pt(V2s[i], Vpts);
+    v_norm_ground(V2s[i]);
+    H2s[i] = gsl_matrix_alloc(STATESIZE, STATESIZE);
+    set_hamiltonian_circular(H2s[i], V2s[i], PLANCK, MASS, HSTEP);
+  }
 
-  evolve(Vs, Hs, Uall);
+  solve_stationary(V1s[0], H1s[0]);
+  solve_stationary(V1s[NSTEPS-1], H1s[NSTEPS-1]);
+  
+  fade_timeevol *Ufade1 = make_fade_timeevol(H1s);
+  fade_timeevol *ufade2 = make_fade_timeevol(H2s);
+
+  evolve(V1s, H1s, Ufade1);
 }
 
 void test_6pts(void)
@@ -121,7 +134,7 @@ void solve_stationary(const gsl_vector *V, gsl_matrix *H)
   
   gsl_vector *Vdisp = gsl_vector_alloc(V->size);
 			         
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 4; i++) {
     double E = get_energy(H, HSTEP, psis[i]);
 
     printf("\033[2J\033[H");
@@ -145,13 +158,11 @@ void solve_stationary(const gsl_vector *V, gsl_matrix *H)
 #define EVOLVE_STATE 1
 #define TFINAL 100.0
 
-#define TON 20.0
-#define TOFF 40.0
-#define TPERSTEP (0.1 / TSTEP)
+#define TPERSTEP ((1.0 / 32.0) / TSTEP)
 
-int vstep_for_tstep(int tstep) {
-  const int tstep_on = tstep - (TON / TSTEP);
-  const int tstep_off = tstep - (TOFF / TSTEP);
+int vstep_for_tstep(double Ton, double Toff, int tstep) {
+  const int tstep_on = tstep - (Ton / TSTEP);
+  const int tstep_off = tstep - (Toff / TSTEP);
   if (tstep_on < 0) { return 0; }
   else if (tstep_off < 0) {
     const int nstep = tstep_on / TPERSTEP;
@@ -198,35 +209,9 @@ void v_norm_ground(gsl_vector *V)
   gsl_vector_add_constant(V, -Eg);
 }
 
-// Integral of (1 - cos th) over a full cycle = +1
-// Need -(1.0/3.0) over all points
-void v_sin(gsl_vector *V, double scale)
-{
-  const int sixth = V->size / 6;
-  for (int i = 0; i < sixth; i++) {
-    gsl_vector_set(V, i, -(1.0/3.0) * scale);
-  }
-  for (int i = sixth; i < 3*sixth; i++) {
-    double th = M_PI * ((double) (i -sixth)) / ((double) sixth);
-    gsl_vector_set(V, i, scale * ((2.0/3.0) - cos(th)));
-  }
-  for (int i = 3*sixth; i < V->size; i++) {
-    gsl_vector_set(V, i, -(1.0/3.0) * scale);
-  }
-}
+#define TFADE1 (10.0)
 
-/*
-    vtstep(V, tstep);
-    set_hamiltonian_circular(Hprev, V, PLANCK, MASS, HSTEP);
-    vtstep(V, tstep+1);
-    set_hamiltonian_circular(Hnext, V, PLANCK, MASS, HSTEP);
-    
-    set_timeevol_halves(U, Hprev, Hnext, PLANCK, TSTEP, NULL);
-
-    timeevol_state(psinew, U, psi);
-*/
-
-void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const precomputed_timeevol *Uall)
+void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const fade_timeevol *Ufade1)
 {
   gsl_vector *V = gsl_vector_calloc(STATESIZE);
   
@@ -245,8 +230,10 @@ void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const pr
   }
 
   psi = gsl_vector_complex_calloc(STATESIZE);
+
+  FILE *f = fopen("circdata/evolve-states.txt", "w");
   
-  if (1) {
+  if (0) {
     gsl_vector_complex_memcpy(psi, psis[0]);
   } else {
     gsl_vector_complex *psi0 = gsl_vector_complex_calloc(STATESIZE);
@@ -276,12 +263,16 @@ void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const pr
   
   for (int tstep = 0; (tstep * TSTEP) <= TFINAL; tstep++) {
     const double t = tstep * TSTEP;
-    const int vstep_curr = vstep_for_tstep(tstep);
+    const int vstep_curr = vstep_for_tstep(5.0, 25.0, tstep);
 
     if (tstep % WRITEEVERY == 0) {
+      fprintf(f, "%d", tstep);
       for (int i = 0; i < STATESIZE; i++) {
         gsl_blas_zdotc(psis[i], psi, gsl_vector_complex_ptr(psieig, i));
+        fprintf(f, "\t%0.6f", gsl_complex_abs(gsl_vector_complex_get(psieig, i))*HSTEP);
       }
+      fprintf(f, "\n");
+      fflush(f);
       gsl_vector_complex_scale(psieig, hstep_c);
 
       gsl_vector_memcpy(Vdisp, Vs[vstep_curr]);
@@ -301,16 +292,16 @@ void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const pr
       terminal_graph_raw(Vdisp, 16, '%');
     }
 
-    const int vstep_next = vstep_for_tstep(tstep+1);
+    const int vstep_next = vstep_for_tstep(5.0, 25.0, tstep+1);
 
     const timeevol_halves *U;
 
     if (vstep_next == vstep_curr) {
-      U = Uall->Usteady[vstep_curr];
+      U = Ufade1->Usteady[vstep_curr];
     } else if (vstep_next == vstep_curr + 1) {
-      U = Uall->Uupfrom[vstep_curr];
+      U = Ufade1->Uupfrom[vstep_curr];
     } else if (vstep_next == vstep_curr - 1) {
-      U = Uall->Udownto[vstep_next];
+      U = Ufade1->Udownto[vstep_next];
     } else {
       fprintf(stderr, "tstep = %d, vstep_curr = %d, vstep_next = %d\n",
 	    tstep, vstep_curr, vstep_next);
@@ -323,9 +314,9 @@ void evolve(gsl_vector *const Vs[NSTEPS], gsl_matrix *const Hs[NSTEPS], const pr
   }
 }
 
-precomputed_timeevol *precompute(gsl_matrix *const Hs[NSTEPS])
+fade_timeevol *make_fade_timeevol(gsl_matrix *const Hs[NSTEPS])
 {
-  precomputed_timeevol *Uall = calloc(1, sizeof(precomputed_timeevol));
+  fade_timeevol *Uall = calloc(1, sizeof(fade_timeevol));
 
   for (int i = 0; i < NSTEPS; i++) {
     Uall->Usteady[i] = timeevol_halves_alloc(STATESIZE);
@@ -343,3 +334,24 @@ precomputed_timeevol *precompute(gsl_matrix *const Hs[NSTEPS])
 
   return Uall;
 }
+
+// Verifying constants, etc.
+// \psi_1(x) = a sin(x)
+// <\psi_1'|\psi_1> = a^2 \int_0^{2\pi} sin^2(x)
+//   = a^2 \int_0^{2\pi} 1/2 (1 - cos(2x))
+//   = a^2 1/2 (2\pi)
+//   = a^2 \pi
+// and so a = 1/sqrt(pi) = 1.0 / M_SQRTPI 
+// <\psi_1'|H|\psi_1> = \int_0^{2\pi} dx a sin (x) (-\hbar^2)/(2m) (a sin(x))''
+//   =  \int_0^{2\pi} dx a sin(x) (-\hbar^2)/(2m) (-a sin(x))
+//   =  \int_0^{2\pi} dx a^2 (\hbar^2)/(2m) sin^2(x)
+//   =  \int_0^{2\pi} dx (1/\pi) (\hbar^2)/(2m) (1/2) (1 - cos(2x))
+//   = (1/\pi) (\hbar^2)/(2m) (1/2) (2\pi) - \int_0^{2\pi} dx ... cos(2x)
+//   = (\hbar^2)/(2m)
+// And for a sin(kx), a=(\pi)^{-1/2} and E = k^2 E_1
+// Frequency, E \Psi(t) = i \hbar \Psi(t)'
+//   \Psi(t)' = (- i E / \hbar) \Psi(t)
+//   \Psi(t) = \Psi(0) exp(-i (E / \hbar) t)
+//   \omega = E / \hbar and \omega_1 = \hbar/(2m)
+// Bigger \hbar or smaller m => faster frequency
+// \hbar 4.0 and m 1.0 means \omega 2 and period 4 \pi
