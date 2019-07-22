919de3b2d4fa2a202c209849ca2c3d57893836b8
Nicholas Ingolia <nick@ingolia.org>
2019-07-22 08:38:40 -0700

    Create and use time evolution matrices

--- rust/wave/src/main.rs
+++ rust/wave/src/main.rs
@@ -1,10 +1,9 @@
 extern crate accelerate_src;
 
-use std::f64::consts;
+use std::io::Write;
 
-use blas::*;
-use lapack::*;
 use num_complex::*;
+use num_traits::zero;
 
 mod linalg;
 mod sch;
@@ -12,95 +11,42 @@ mod sch;
 use linalg::*;
 
 fn main() {
-    let a = vec![
-        Complex::new(1.0, 0.0),
-        Complex::new(0.0, 0.0),
-        Complex::new(0.0, 0.0),
-        Complex::new(0.0, 0.0),
-        Complex::new(0.0, consts::FRAC_1_SQRT_2),
-        Complex::new(0.0, -consts::FRAC_1_SQRT_2),
-        Complex::new(0.0, 0.0),
-        Complex::new(0.0, consts::FRAC_1_SQRT_2),
-        Complex::new(0.0, consts::FRAC_1_SQRT_2),
-    ];
-
-    let b = vec![
-        Complex::new(1.0, 0.0),
-        Complex::new(2.0, 0.0),
-        Complex::new(3.0, 0.0),
-    ];
-
-    let mut c = vec![Complex::new(-1.0, -1.0); 3];
-
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{:?}", c);
-
-    unsafe {
-        zgemv(
-            b'N',
-            3,
-            3,
-            Complex::new(1.0, 0.0),
-            &a,
-            3,
-            &b,
-            1,
-            Complex::new(0.0, 0.0),
-            &mut c,
-            1,
-        );
-    }
-
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{:?}", c);
-
-    let m = MatrixSquare::from_rows(
-        &vec![NVector::row_from_vec(vec![ 1.0,  1.0,  0.0,  0.0 ]),
-              NVector::row_from_vec(vec![ 1.0, -1.0,  0.0,  0.0 ]),
-              NVector::row_from_vec(vec![ 0.0,  0.0,  1.0, -1.0 ]),
-              NVector::row_from_vec(vec![ 0.0,  0.0, -1.0,  1.0 ])]);
-
-    println!("{:6.3}", m);
+    let n = 40;
+    let model = sch::ModelS1::new(sch::PLANCK_DEFAULT, sch::MASS_DEFAULT, sch::LENGTH_DEFAULT);
+    let m = model.hamiltonian(&NVector::row_from_vec(vec![0.0; n]));
+    let tstep = 1.0 / 128.0;
     
-    let (eigvals, eigvecs) = m.dsyev();
+    let timeevol = sch::make_time_evol(&m, tstep / sch::PLANCK_DEFAULT);
 
-    println!("{:.4}", NVector::row_from_vec(eigvals));
+    let mut times = Vec::new();
+    let mut psis = Vec::new();
 
-    for eigvec in eigvecs {
-        println!("{:7.4}", eigvec);
-    }
-
-    let n = 12;
-    let model = sch::ModelS1::new(sch::H_DEFAULT, sch::MASS_DEFAULT, sch::LENGTH_DEFAULT);
-    let m = model.hamiltonian_spinor(n);
-    println!("{:6.3}", m);
-
-    let (eigvals, eigvecs) = m.dsyev();
-
-    println!("{:.4}", NVector::row_from_vec(eigvals));
-
-    for eigvec in eigvecs.iter() {
-        println!("{:6.3}", eigvec);
-    }
-
-    let mut v = NVector::col_from_vec(vec![1.0; n]);
-    v.scale(v.norm2().recip());
-    println!("{:6.3}", v.dagger());
-
-    for eigvec in eigvecs.iter() {
-        println!("{:6.3}", eigvec.dot(&v));
-    }
+    let mut psi: NVector<Complex64, Col> = NVector::col_from_vec(vec![zero(); n]);
 
     for i in 0..n {
-        let x = 1.0 * std::f64::consts::PI * (i as f64) / (n as f64);
-        v[i] = x.sin();
+        let x = 2.0 * std::f64::consts::PI * (i as f64) / (n as f64);
+        psi[i] = Complex64::from(x.sin());
     }
-    v.scale(v.norm2().recip());
-    println!("{:6.3}", v.dagger());
+    psi.scale(Complex64::from(psi.norm2().recip()));
+    
+    for i in 0..1024 {
+        psi = timeevol.mmulv(&psi);
+        psis.push(psi.clone());
+        times.push(i as f64 * tstep);
+    }
 
-    for eigvec in eigvecs.iter() {
-        println!("{:6.3}", eigvec.dot(&v));
+    let mut fout = std::fs::File::create("res.csv").unwrap();
+    write!(fout, "x").unwrap();
+    for i in 0..psis.len() {
+        write!(fout, ",a{:05},ph{:05}", i, i).unwrap();
+    }
+    write!(fout, "\n").unwrap();
+    for j in 0..n {
+        write!(fout, "{}", j).unwrap();
+        for i in 0..psis.len() {
+            let x = psis[i][j];
+            write!(fout, ",{:6.3},{:6.3}", x.norm_sqr(), x.arg()).unwrap();
+        }
+        write!(fout, "\n").unwrap();
     }
 }
--- rust/wave/src/sch.rs
+++ rust/wave/src/sch.rs
@@ -1,43 +1,74 @@
+use num_complex::*;
+
 use crate::linalg::*;
 
-pub const H_DEFAULT: f64 = 4.0;
+pub const PLANCK_DEFAULT: f64 = 4.0;
 pub const MASS_DEFAULT: f64 = 1.0;
 pub const LENGTH_DEFAULT: f64 = (2.0 * std::f64::consts::PI);
 
 pub struct ModelS1 {
-    h: f64,
+    planck: f64,
     mass: f64,
     length: f64,
 }
 
 impl ModelS1 {
-    pub fn new(h: f64, mass: f64, length: f64) -> Self {
-        ModelS1 { h: h, mass: mass, length: length }
+    pub fn new(planck: f64, mass: f64, length: f64) -> Self {
+        ModelS1 { planck: planck, mass: mass, length: length }
     }
 
-    pub fn hamiltonian(&self, n: usize) -> MatrixSquare<f64> {
+    pub fn hamiltonian<T>(&self, v: &NVector<f64, T>) -> MatrixSquare<f64> {
+        let n = v.len();
         let hstep = self.length / (n as f64);
 
         let mut hamil = MatrixSquare::zeros(n);
 
-        let pfact = -0.5 * self.h * self.h / self.mass;
+        let pfact = -0.5 * self.planck * self.planck / self.mass;
         let hstep2 = 1.0 / (hstep * hstep);
 
         for j in 0..n {
             hamil[(j, (j+n-1)%n)] = pfact * hstep2;
             hamil[(j, (j+n+1)%n)] = pfact * hstep2;
-            hamil[(j, j)] = -2.0 * pfact * hstep2;
+            hamil[(j, j)] = -2.0 * pfact * hstep2 + v[j];
         }
 
         hamil
     }
 
-    pub fn hamiltonian_spinor(&self, n: usize) -> MatrixSquare<f64> {
-        let mut hamil = self.hamiltonian(n);
-
+    pub fn hamiltonian_spinor<T>(&self, v: &NVector<f64, T>) -> MatrixSquare<f64> {
+        let mut hamil = self.hamiltonian(v);
+        let n = v.len();
+        
         hamil[(0,n-1)] = -hamil[(0,n-1)];
         hamil[(n-1,0)] = -hamil[(n-1,0)];
 
         hamil
     }    
+}
+
+pub fn make_time_evol(hamil: &MatrixSquare<f64>, tstep_over_planck: f64) -> MatrixSquare<Complex64> {
+    // println!("H =\n{:6.3}", hamil);
+
+    let (eigvals, eigvecs) = hamil.dsyev();
+
+    let eigvecinv: MatrixSquare<Complex64> = eigvecs.into();
+    
+    let eigvecfwd = eigvecinv.dagger();
+
+    // println!("eigvecfwd =\n{:13.3}", eigvecfwd);
+    // println!("eigvecinv =\n{:13.3}", eigvecinv);
+
+    let mut eigvalmat = MatrixSquare::zeros(eigvals.len());
+    for (i, eigval) in eigvals.iter().enumerate() {
+        eigvalmat[(i, i)] = (-Complex64::i() * f64::from(eigval * tstep_over_planck)).exp();
+    }
+    // println!("eigvalmat =\n{:13.3}", eigvalmat);
+
+    let timeevol = eigvecinv.mmulm(&eigvalmat).mmulm(&eigvecfwd);
+
+    // println!("timeevol =\n{:13.3}", timeevol);
+
+    // println!("timeevol^H timeevol =\n{:13.3}", timeevol.dagger().mmulm(&timeevol));
+
+    timeevol
 }
