f05168282b7c4416ac85046f5f6c3f0d9afe3d51
Nicholas Ingolia <nick@ingolia.org>
2019-07-23 08:25:59 -0700

    Test transfer of states between bases

--- rust/wave/src/main.rs
+++ rust/wave/src/main.rs
@@ -15,6 +15,32 @@ fn main() {
     let model = sch::ModelS1::new(sch::PLANCK_DEFAULT, sch::MASS_DEFAULT, sch::LENGTH_DEFAULT);
     let m = model.hamiltonian(&NVector::row_from_vec(vec![0.0; n]));
     let tstep = 1.0 / 128.0;
+
+    let ssa = sch::stationary_states(&m);
+    sch::write_stationary("psi-a.csv", "e-a.csv", &m);
+
+    let xsin: Vec<f64> = (0..n).map(|k| 2.0 * std::f64::consts::PI * (k as f64) / (n as f64)).collect();
+    let vsin: Vec<f64> = xsin.iter().map(|x| 2.0 * x.sin()).collect();
+    let mb = model.hamiltonian(&NVector::row_from_vec(vsin));
+    let ssb = sch::stationary_states(&mb);
+    sch::write_stationary("psi-b.csv", "e-b.csv", &mb);
+
+    for (i, (ebi, vbi)) in ssb.iter().enumerate() {
+        println!("{:02}\t{:0.4}\t{:0.4}", i, ebi, ssa[0].1.dagger().dot(vbi));
+    }
+
+    let a0b = ssa[0].1.dagger().dot(&ssb[0].1);
+    let a2b = ssa[0].1.dagger().dot(&ssb[2].1);
+    // let sum = 
+
+    let egap = ssb[2].0 - ssb[0].0;
+    let time = 0.5 * std::f64::consts::PI / egap;
+    println!("egap = {:0.3}, time = {:0.3}", egap, time);
+    let tegap = sch::make_time_evol(&m, time);
+    let mut psi: NVector<Complex64,Col> = ssa[0].1.clone().into();
+    println!("{:?}", psi.dagger());
+    psi = tegap.mmulv(&psi);
+    println!("{:?}", psi.dagger());
     
     let timeevol = sch::make_time_evol(&m, tstep / sch::PLANCK_DEFAULT);
 
